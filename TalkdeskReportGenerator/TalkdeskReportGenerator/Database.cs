e2c939d2194adb9da032228b552a9afe87a06599
bill.runge <bill.runge@P-CS-DSK-BRUN>
2018-10-23 22:38:01 -0500

    Added a class to create database connecion

---
+++ TalkdeskReportGenerator/TalkdeskReportGenerator/Database.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TalkdeskReportGenerator
+{
+    public class Database
+    {
+        private readonly SqlConnection _connection;
+        private readonly string _dataSource = "IL1IPRFLCTDB005";
+        private readonly string _database = "Talkdesk";
+        private readonly int _timeout = 30;
+
+
+        public Database()
+        {
+            _connection = new SqlConnection($"Server={_dataSource};" +
+                                            $"Database={_database};" +
+                                            $"Integrated Security=True;" +
+                                            $"Connection Timeout={_timeout};");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                _connection.Open();
+            }
+            catch (Exception e)
+            {
+                //need better error handling
+                Console.WriteLine(e.ToString());
+            }
+
+            return _connection;
+        }
+    }
+}
