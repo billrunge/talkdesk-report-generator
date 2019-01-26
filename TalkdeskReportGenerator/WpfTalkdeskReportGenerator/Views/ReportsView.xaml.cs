c801a29629730a7469c7657100a92781a021f584
billrunge <billrunge@gmail.com>
2019-01-26 12:01:36 -0600

    Adding the rough framework to make it so the ViewModel for running the report isn't the shell view model, so the user can switch between the shell view model and the settings page without a pop up

--- TalkdeskReportGenerator/WpfTalkdeskReportGenerator/Views/ReportsView.xaml.cs
+++ TalkdeskReportGenerator/WpfTalkdeskReportGenerator/Views/ReportsView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfTalkdeskReportGenerator.Views
 {
     /// <summary>
-    /// Interaction logic for SettingsView.xaml
+    /// Interaction logic for ReportsView.xaml
     /// </summary>
-    public partial class SettingsView
+    public partial class ReportsView : UserControl
     {
-        public SettingsView()
+        public ReportsView()
         {
             InitializeComponent();
         }
