2e2fe67a9194e22abd5906620a3f8b4b38ca8028
billrunge <billrunge@gmail.com>
2019-01-27 11:41:22 -0600

    Removed unushed project folders from solution

--- TalkdeskReportGenerator/ConsoleTalkdeskReportGenerator/ExcelRowRange.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleTalkdeskReportGenerator
-{
-    class ExcelRowRange
-    {
-        public int FirstValue { get; set; }
-        public int SecondValue { get; set; }
-    }
-}
--- TalkdeskReportGenerator/ConsoleTalkdeskReportGenerator/Status.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleTalkdeskReportGenerator
-{
-    class Status
-    {
-        public string DayName { get; set; }
-        public string StatusLabel { get; set; }
-        public int StatusTime { get; set; }
-    }
-}
