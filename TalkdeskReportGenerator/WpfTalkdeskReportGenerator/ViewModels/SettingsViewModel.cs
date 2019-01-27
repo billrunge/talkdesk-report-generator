c78cab3e623ecb65fcc47a8ec885f54324d93919
billrunge <billrunge@gmail.com>
2019-01-27 00:32:20 -0600

    Played around with styling of ReportsViewModel and SettingsViewModel. Also turned ShellViewModel into a "ToolWindow" to make the display cleaner.

--- TalkdeskReportGenerator/WpfTalkdeskReportGenerator/ViewModels/SettingsViewModel.cs
+++ TalkdeskReportGenerator/WpfTalkdeskReportGenerator/ViewModels/SettingsViewModel.cs
@@ -10,44 +10,160 @@ using WpfTalkdeskReportGenerator.Models;
 namespace WpfTalkdeskReportGenerator.ViewModels
 {
     public class SettingsViewModel : Screen
-    {
-        private SettingsModel _settings;
-        public SettingsModel Settings {
+    { 
+        private TimeZoneInfo _selectedTimeZone;
+        private int _phoneColorKeyRow;
+        private int _groupByNameRow;
+        private string _phoneColorKeyColumn;
+        private string _groupByNameColumn;
+        private string _agentNameColumn;
+        private string _twelveAmColumn;
+
+        public TimeZoneInfo SelectedTimeZone
+        {
             get
             {
-                return _settings;
+                return _selectedTimeZone;
             }
             set
             {
-                _settings = value;
-                NotifyOfPropertyChange(() => Settings);
+                _selectedTimeZone = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => SelectedTimeZone);
+            }
+        }
+        public int PhoneColorKeyRow
+        {
+            get
+            {
+                return _phoneColorKeyRow;
+            }
+            set
+            {
+                _phoneColorKeyRow = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => PhoneColorKeyRow);
+            }
+        }
+        public int GroupByNameRow
+        {
+            get
+            {
+                return _groupByNameRow;
+            }
+            set
+            {
+                _groupByNameRow = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => GroupByNameRow);
+            }
+        }
+        public string PhoneColorKeyColumn
+        {
+            get
+            {
+                return _phoneColorKeyColumn;
+            }
+            set
+            {
+                _phoneColorKeyColumn = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => PhoneColorKeyColumn);
+            }
+        }
+        public string GroupByNameColumn
+        {
+            get
+            {
+                return _groupByNameColumn;
+            }
+            set
+            {
+                _groupByNameColumn = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => GroupByNameColumn);
+            }
+        }
+        public string AgentNameColumn
+        {
+            get
+            {
+                return _agentNameColumn;
+            }
+            set
+            {
+                _agentNameColumn = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => AgentNameColumn);
+
+            }
+        }
+        public string TwelveAmColumn
+        {
+            get
+            {
+                return _twelveAmColumn;
+            }
+            set
+            {
+                _twelveAmColumn = value;
+                NotifyOfPropertyChange(() => CanSave);
+                NotifyOfPropertyChange(() => CanCancel);
+                NotifyOfPropertyChange(() => TwelveAmColumn);
             }
         }
 
         public BindableCollection<TimeZoneInfo> TimeZoneInfos { get; set; }
 
+        public bool CanSave
+        {
+            get
+            {
+                return HaveSettingsChanged();
+            }
+        }
+        public bool CanCancel
+        {
+            get
+            {
+                return HaveSettingsChanged();
+            }
+        }
+
+
+        private bool HaveSettingsChanged()
+        {
+            bool hasChanges = false;
+            if (Properties.Settings.Default.TimeZoneId != SelectedTimeZone.Id ||
+            Properties.Settings.Default.PhoneColorKeyColumn != PhoneColorKeyColumn ||
+            Properties.Settings.Default.PhoneColorKeyRow != PhoneColorKeyRow ||
+            Properties.Settings.Default.GroupByNameColumn != GroupByNameColumn ||
+            Properties.Settings.Default.GroupByNameRow != GroupByNameRow ||
+            Properties.Settings.Default.AgentNameColumn != AgentNameColumn ||
+            Properties.Settings.Default.TwelveAmColumn != TwelveAmColumn)
+            {
+                hasChanges = true;
+            }
+            return hasChanges;
+        }
+
+
         public SettingsViewModel()
         {
             TimeZoneInfos = GetTimeZoneInfoList();
-            Settings = new SettingsModel
-            {
-                ExcelTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(Properties.Settings.Default.TimeZoneId),
-                PhoneColorKeyCell = new ColumnRow
-                {
-                    Column = Properties.Settings.Default.PhoneColorKeyColumn,
-                    Row = Properties.Settings.Default.PhoneColorKeyRow
-                },
-                GroupByNameCell = new ColumnRow
-                {
-                    Column = Properties.Settings.Default.GroupByNameColumn,
-                    Row = Properties.Settings.Default.GroupByNameRow
-                },
-                AgentNameColumn = Properties.Settings.Default.AgentNameColumn,
-                TwelveAmColumn = Properties.Settings.Default.TwelveAmColumn
-            };
-
-
-
+            SelectedTimeZone = TimeZoneInfo.FindSystemTimeZoneById(Properties.Settings.Default.TimeZoneId);
+            PhoneColorKeyRow = Properties.Settings.Default.PhoneColorKeyRow;
+            PhoneColorKeyColumn = Properties.Settings.Default.PhoneColorKeyColumn;
+            GroupByNameRow = Properties.Settings.Default.GroupByNameRow;
+            GroupByNameColumn = Properties.Settings.Default.GroupByNameColumn;
+            AgentNameColumn = Properties.Settings.Default.AgentNameColumn;
+            TwelveAmColumn = Properties.Settings.Default.TwelveAmColumn;
         }
 
         private List<string> GetTimeZoneList()
@@ -74,6 +190,21 @@ namespace WpfTalkdeskReportGenerator.ViewModels
             return timeZoneInfos;
         }
 
+        public void Cancel()
+        {
+            SelectedTimeZone = TimeZoneInfo.FindSystemTimeZoneById(Properties.Settings.Default.TimeZoneId);
+            PhoneColorKeyRow = Properties.Settings.Default.PhoneColorKeyRow;
+            PhoneColorKeyColumn = Properties.Settings.Default.PhoneColorKeyColumn;
+            GroupByNameRow = Properties.Settings.Default.GroupByNameRow;
+            GroupByNameColumn = Properties.Settings.Default.GroupByNameColumn;
+            AgentNameColumn = Properties.Settings.Default.AgentNameColumn;
+            TwelveAmColumn = Properties.Settings.Default.TwelveAmColumn;
+            Properties.Settings.Default.Save();
+            NotifyOfPropertyChange(() => CanSave);
+            NotifyOfPropertyChange(() => CanCancel);
+        }
+
+
 
 
         public void Back()
@@ -82,14 +213,16 @@ namespace WpfTalkdeskReportGenerator.ViewModels
         }
         public void Save()
         {
-            Properties.Settings.Default.TimeZoneId = Settings.ExcelTimeZoneInfo.Id;
-            Properties.Settings.Default.PhoneColorKeyColumn = Settings.PhoneColorKeyCell.Column;
-            Properties.Settings.Default.PhoneColorKeyRow = Settings.PhoneColorKeyCell.Row;
-            Properties.Settings.Default.GroupByNameColumn = Settings.GroupByNameCell.Column;
-            Properties.Settings.Default.GroupByNameRow = Settings.GroupByNameCell.Row;
-            Properties.Settings.Default.AgentNameColumn = Settings.AgentNameColumn;
-            Properties.Settings.Default.TwelveAmColumn = Settings.TwelveAmColumn;
+            Properties.Settings.Default.TimeZoneId = SelectedTimeZone.Id;
+            Properties.Settings.Default.PhoneColorKeyColumn = PhoneColorKeyColumn;
+            Properties.Settings.Default.PhoneColorKeyRow = PhoneColorKeyRow;
+            Properties.Settings.Default.GroupByNameColumn = GroupByNameColumn;
+            Properties.Settings.Default.GroupByNameRow = GroupByNameRow;
+            Properties.Settings.Default.AgentNameColumn = AgentNameColumn;
+            Properties.Settings.Default.TwelveAmColumn = TwelveAmColumn;
             Properties.Settings.Default.Save();
+            NotifyOfPropertyChange(() => CanSave);
+            NotifyOfPropertyChange(() => CanCancel);
         }
     }
 }
